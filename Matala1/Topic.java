52ce06daeed94ec5c3753aabacad6af50961f095
emily-belenky <136440047+emily-belenky@users.noreply.github.com>
2024-12-14 17:35:38 +0200

    Update Topic.java

--- Matala1/Topic.java
+++ Matala1/Topic.java
@@ -1,27 +1,59 @@
-package project_biu.graph;
+package graph;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-public class Topic {
+class Topic {
+
     public final String name;
-    Topic(String name){
-        this.name=name;
+    private final List<Agent> subscribers;
+    private final List<Agent> publishers;
+
+    private String result = "";
+
+    Topic(String name) {
+        this.name = name;
+        this.subscribers = new CopyOnWriteArrayList<>();
+        this.publishers = new CopyOnWriteArrayList<>();
     }
 
-    public void subscribe(Agent a){
-    }
-    public void unsubscribe(Agent a){
-    }
-
-    public void publish(Message m){
+    public void subscribe(Agent agent) {
+        this.subscribers.add(agent);
     }
 
-    public void addPublisher(Agent a){
+    public void unsubscribe(Agent agent) {
+        this.subscribers.remove(agent);
     }
 
-    public void removePublisher(Agent a){
+    public void publish(Message message) {
+        setResult(message.asText);
+
+        for (Agent agent : this.subscribers) {
+            agent.callback(this.name, message);
+        }
     }
 
+    public void addPublisher(Agent agent) {
+        this.publishers.add(agent);
+    }
 
+    public void removePublisher(Agent agent) {
+        this.publishers.remove(agent);
+    }
+
+    public List<Agent> getPublishers() {
+        return this.publishers;
+    }
+
+    public List<Agent> getSubscribers() {
+        return this.subscribers;
+    }
+
+    public String getResult() {
+        return this.result;
+    }
+
+    public void setResult(String anyResult) {
+        this.result = anyResult;
+    }
 }
