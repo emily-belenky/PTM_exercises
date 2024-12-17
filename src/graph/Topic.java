666fe1e7512c424f16e7260ce6ccbc1d6ea4d81f
emily-belenky <136440047+emily-belenky@users.noreply.github.com>
2024-12-17 21:10:33 +0200

    Update Topic.java

--- src/graph/Topic.java
+++ src/graph/Topic.java
@@ -1,58 +1,51 @@
-package graph;
+package test;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-/// Advanced Programming exercise 1
+class Topic {
 
-
-public class Topic {
-
-    // Fields
     public final String name;
-    private List<Agent> subscribers;
-    private List<Agent> publishers;
+    private final List<Agent> subscribers;
+    private final List<Agent> publishers;
 
     private String result = "";
 
-    // Constructor
-    Topic(String name){
+    Topic(String name) {
         this.name = name;
-        this.subscribers = new ArrayList<Agent>();
-        this.publishers = new ArrayList<Agent>();
+        this.subscribers = new CopyOnWriteArrayList<>();
+        this.publishers = new CopyOnWriteArrayList<>();
     }
 
-    // Methods
-    public void subscribe(Agent agent){
+    public void subscribe(Agent agent) {
         this.subscribers.add(agent);
     }
-    public void unsubscribe(Agent agent){
+
+    public void unsubscribe(Agent agent) {
         this.subscribers.remove(agent);
     }
 
-    public void publish(Message message){
-
-        // Update result
+    public void publish(Message message) {
         setResult(message.asText);
 
-        for(Agent agent : this.subscribers){
+        for (Agent agent : this.subscribers) {
             agent.callback(this.name, message);
         }
     }
 
-    public void addPublisher(Agent agent){
+    public void addPublisher(Agent agent) {
         this.publishers.add(agent);
     }
 
-    public void removePublisher(Agent agent){
+    public void removePublisher(Agent agent) {
         this.publishers.remove(agent);
-
     }
 
-    public List<Agent> getPublishers(){
+    public List<Agent> getPublishers() {
         return this.publishers;
     }
-    public List<Agent> getSubscribers(){
+
+    public List<Agent> getSubscribers() {
         return this.subscribers;
     }
 
@@ -60,7 +53,7 @@ public class Topic {
         return this.result;
     }
 
-    public void setResult(String anyResult){
+    public void setResult(String anyResult) {
         this.result = anyResult;
     }
 }
