83f82e6215ed2849300dfa4183e3068c9dfbb964
emily-belenky <136440047+emily-belenky@users.noreply.github.com>
2024-12-17 21:08:46 +0200

    Update Graph.java

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,141 +1,52 @@
-package graph;
+package test;
 
-import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-/// Advanced Programming exercise 3
+public class Graph extends ArrayList<Node> {
 
+    private Integer agentCounter = 1;
 
-import graph.TopicManagerSingleton.TopicManager;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-
-
-public class Graph extends ArrayList<Node>{
-
-	// Data members
-	private Integer agentCounter = 1;
-
-
-	// Methods
-
-	// Detect Cycle in graph
     public boolean hasCycles() {
-       for(Node node : this) {
-    	   if(node.hasCycles() == true)
-    		   return true;
-       }
-       return false;
-    }
-    
-    // Method to initialize the graph from the topics
-    public void createFromTopics() {
-
-    	this.clear();
-    	
-    	// Map each seen agent to a node
-    	Map<Agent, Node> agentSeen = new HashMap<Agent, Node>();
-    	
-    	// Iterate over the topics
-        for (Topic topic : TopicManagerSingleton.get().getTopics()) {
-           
-        	// Create topic node
-        	Node topicNode = new Node("T" + topic.name);
-        	
-        	// Iterate over the topic subscribers
-        	for(Agent agent : topic.getSubscribers()) {
-        		
-        		// If not seen this agent yet
-        		if(agentSeen.containsKey(agent) == false){
-        			
-        			// Add this as node to the graph
-					Node agentNode = new Node("A" + this.agentCounter.toString() + " " + agent.getName());
-        			this.add(agentNode);
-
-					agentCounter += 1;
-        			// Mark as seen
-        			agentSeen.put(agent, agentNode);
-        		}
-        		// Set agent node as the current topic node neighbor
-        		topicNode.addEdge(agentSeen.get(agent));
-        		
-        	}
-        	// Iterate over the topic publishers
-        	for(Agent agent : topic.getPublishers()) {
-        		
-        		// If not seen this agent yet
-        		if(agentSeen.containsKey(agent) == false){
-        			// Add this as node to the graph
-					Node agentNode = new Node("A" + this.agentCounter.toString() + " " + agent.getName());
-        			this.add(agentNode);
-
-					agentCounter += 1;
-        			// Mark as seen
-        			agentSeen.put(agent, agentNode);
-        		}
-        		// Set topic node as the agent node neighbor
-        		agentSeen.get(agent).addEdge(topicNode);
-        		
-        	}
-        	// Add topic node to graph
-        	this.add(topicNode);
-        	
+        for (Node node : this) {
+            if (node.hasCycles()) {
+                return true;
+            }
         }
-
+        return false;
     }
 
-	// Method to generate XML from the graph
-	public String generateXML() throws Exception {
-		// Create a DocumentBuilderFactory
-		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+    public void createFromTopics() {
+        this.clear();
+        Map<Agent, Node> agentSeen = new HashMap<>();
 
-		// Create a new Document
-		Document doc = docBuilder.newDocument();
+        for (Topic topic : TopicManagerSingleton.get().getTopics()) {
+            // Set topic node name as "T<topic name>"
+            Node topicNode = new Node("T" + topic.name);
+            this.add(topicNode);
 
-		// Create the root element
-		Element rootElement = doc.createElement("graph");
-		doc.appendChild(rootElement);
+            // Handle subscribers
+            for (Agent agent : topic.getSubscribers()) {
+                agentSeen.computeIfAbsent(agent, a -> {
+                    String agentName = "A" + a.getName(); // Set name as "A<agent name>"
+                    Node agentNode = new Node(agentName);
+                    this.add(agentNode);
+                    return agentNode;
+                });
+                topicNode.addEdge(agentSeen.get(agent));
+            }
 
-		// Iterate through nodes in the graph
-		for (Node node : this) {
-			// Create node element
-			Element nodeElement = doc.createElement("node");
-			nodeElement.setAttribute("id", node.getName());
-			rootElement.appendChild(nodeElement);
-
-			// Add edges
-			for (Node neighbor : node.getEdges()) {
-				Element edgeElement = doc.createElement("edge");
-				edgeElement.setAttribute("to", neighbor.getName());
-				nodeElement.appendChild(edgeElement);
-			}
-		}
-
-		// Convert the Document to a String
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-		DOMSource source = new DOMSource(doc);
-		StringWriter writer = new StringWriter();
-		StreamResult result = new StreamResult(writer);
-		transformer.transform(source, result);
-
-		return writer.toString();
-	}
-
-
-
-
+            // Handle publishers
+            for (Agent agent : topic.getPublishers()) {
+                agentSeen.computeIfAbsent(agent, a -> {
+                    String agentName = "A" + a.getName(); // Set name as "A<agent name>"
+                    Node agentNode = new Node(agentName);
+                    this.add(agentNode);
+                    return agentNode;
+                });
+                agentSeen.get(agent).addEdge(topicNode);
+            }
+        }
+    }
 }
